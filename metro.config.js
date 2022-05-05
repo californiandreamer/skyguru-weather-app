207601259f39502a31faa3308fb7805debbcfd6f
Nazar Sydiaha <nazarsydiaha@ip-192-168-2-238.eu-central-1.compute.internal>
2022-05-05 18:14:51 +0200

    obfuscation plugin added

--- metro.config.js
+++ metro.config.js
@@ -8,6 +8,28 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { getDefaultConfig } = require('metro-config')
 
+const jsoMetroPlugin = require('obfuscator-io-metro-plugin')(
+  {
+    // for these option look javascript-obfuscator library options from  above url
+    compact: false,
+    sourceMap: false,
+    controlFlowFlattening: true,
+    controlFlowFlatteningThreshold: 1,
+    numbersToExpressions: true,
+    simplify: true,
+    stringArrayShuffle: true,
+    splitStrings: true,
+    stringArrayThreshold: 1,
+  },
+  {
+    runInDev: false /* optional */,
+    logObfuscatedFiles: true /* optional generated files will be located at ./.jso */,
+    // source Map generated after obfuscation is not useful right now
+    sourceMapLocation:
+      './index.android.bundle.map' /* optional  only works if sourceMap: true in obfuscation option */,
+  }
+)
+
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
@@ -26,5 +48,6 @@ module.exports = (async () => {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
     },
+    ...jsoMetroPlugin,
   }
 })()
